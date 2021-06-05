bb94201cf11c9d118ce06dc5e0d9c44d826c8ab9
CVSM Group -  email: czhu@bupt.edu.cn <76641042+bupt-ai-cz@users.noreply.github.com>
2021-06-05 21:10:10 +0800

    Add files via upload

---
+++ code/test.sh
@@ -0,0 +1,3 @@
+#! bash 
+
+python3 test.py --logdir logs/Rain100H/ANRN --save_path results/Rain100H/ANRN --data_path datasets/test/Rain100H/rainy
---
+++ code/train.sh
@@ -0,0 +1,4 @@
+#! bash
+
+# Rain100H
+python3 train.py --preprocess True --save_path logs/Rain100H/ANRN --data_path datasets/train/RainTrainH
